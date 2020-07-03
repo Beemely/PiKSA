234b68494dbb6ef8d3a55a12e6cdaac9d0627a3b
Dominik Kacprzak <d.f.kacprzak@gmail.com>
2020-07-03 18:14:53 +0200

    commit

---
+++ WebApp/Services/JsonFileProductService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using WebApp.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApp.Services
+{
+    public class JsonFileProductService
+    {
+
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Products.json"); }
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var JsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(JsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+
+
+
+
+    }
+}
+
+
